a16a0189a7d31ae0682203a893b3484aeaffcc41
Jeff <smartmu@gmail.com>
2014-08-21 22:41:03 -0400

    Update models.py

--- models.py
+++ models.py
@@ -25,8 +25,8 @@ class Item(BaseModel):
     name = CharField(max_length=50)
     spicy = BooleanField(default=False)
     gluten_free = BooleanField(default=False)
-    description = TextField()
-    chinese_name = CharField(max_length=50)
+    description = TextField(null=True)
+    chinese_name = CharField(max_length=50, null=True)
 
 
 class MenuItem(BaseModel):
